b85405e9ff673f50a57f30cf085570145d348225
Abhishek Nayyar <abhisheknayyar@Abhisheks-MacBook-Air.local>
2015-09-19 11:38:12 -0500

    heroku deploy changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,5 @@ module ApplicationHelper
     css_class = column == sort_column ? "hilite" : nil      
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
-
  end
 end
